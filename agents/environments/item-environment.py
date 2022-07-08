aac3ae580c720f455aee928e4ba6819f9b71f3c2
Diego Resendez <diego.resendez@bloksports.com>
2022-07-08 14:21:22 -0500

    [FEATURE] Environment

--- agents/environments/item-environment.py
+++ agents/environments/item-environment.py
@@ -1,9 +1,25 @@
 from gym import Env
 from gym.spaces import Discrete, Box
 
+"""
+Actions: [ Add, Remove, Hold, Adjust ]
+
+Add     : Recomend Add Item to List
+Remove  : Recomend Remove Item to List
+Hold    : Recomend Hold 
+Adjust  : Adjust Parameters
+"""
+actions: Discrete = Discrete(4)
+
+seasons: Discrete = Discrete(4)
+
+observation_space: Box = Box(6,)
+
 class ItemEnv(Env):
     def __init__(self) -> None:
-        super().__init__()
+        self.action_space = actions
+        self.obervation_space = observation_space
+        self.state = 40
         pass
 
     def step(self, action: int) -> tuple:
